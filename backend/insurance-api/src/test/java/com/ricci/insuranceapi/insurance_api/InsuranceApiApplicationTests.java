64bc99542be83e4377abe18c85b18b68ffaf2b72
Pixel Polo <paulr237@gmail.com>
2025-10-27 15:06:14 +0100

    (backend) Remove jdbc autowired bean to apply R__sample-test-data.sql before each test, replaced by the annotation @Sql

--- backend/insurance-api/src/test/java/com/ricci/insuranceapi/insurance_api/InsuranceApiApplicationTests.java
+++ backend/insurance-api/src/test/java/com/ricci/insuranceapi/insurance_api/InsuranceApiApplicationTests.java
@@ -1,39 +1,28 @@
 package com.ricci.insuranceapi.insurance_api;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-
+import org.springframework.test.context.jdbc.Sql;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
+ * Test data is loaded from a reusable migration script for Flyway:
+ * /src/test/resources/db/migration/R__sample-test-data.sql
+ */
+
 @SpringBootTest
+@Sql("/db/migration/R__sample-test-data.sql") // Executed before each test
 public abstract class InsuranceApiApplicationTests {
 
 	protected static final String API_VERSION = System.getProperty("api.version", "v1");
 	protected static final String BASE_PATH = "/api/" + API_VERSION + "/";
 	protected static final boolean VERBOSE = "true".equalsIgnoreCase(System.getProperty("test.verbose", "true"));
 	protected static final Logger LOGGER = LoggerFactory.getLogger(InsuranceApiApplicationTests.class);
-
-	@Autowired
-	protected JdbcTemplate jdbc;
-
-	@BeforeEach
-	void resetDatabase() throws IOException {
-		// Test data is loaded from a reusable migration script for Flyway:
-		// /backend/insurance-api/src/test/resources/db/migration/R__sample-test-data.sql
-		String sql = Files.readString(Paths.get("src/test/resources/db/migration/R__sample-test-data.sql"));
-		jdbc.execute(sql);
-	}
 
 	@Test
 	void contextLoads() {
